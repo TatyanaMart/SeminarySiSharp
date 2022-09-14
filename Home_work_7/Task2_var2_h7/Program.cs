07d7990b801933da7d8453c63ad6730a4e69c58f
Tatyana <sta-rzn62@yandex.ru>
2022-09-14 13:04:22 +0300

    Add Home_work_7

---
+++ Home_work_7/Task2_var2_h7/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет. Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+// Вариант решения с помощью методов
+int[,] matrix = new int[3, 4];
+
+FillPrintArray(matrix);
+
+Console.Write("Введите номер строки: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+GetElement(matrix, row, column);
+
+void FillPrintArray(int[,] matr) // метод заполняет матрицу случайными числами и печатает ее
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 11);
+            Console.Write(matr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetElement(int[,] matr, int row, int column) // метод поиска значения элемента с указанной позицией
+{
+    if (row < matr.GetLength(0) && column < matr.GetLength(1))
+    {
+        Console.WriteLine($"Значение указанного элемента: {matr[row, column]}");
+    }
+    else
+    {
+        Console.WriteLine("Элемент с указанными индексами не найден");
+    }
+}
+
+
+
+
---
+++ Home_work_7/Task3_h7/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+
+FillPrintArray(array);
+GetAverage(array);
+
+void FillPrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 11);
+            Console.Write(matr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetAverage(int[,] matr)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        double sum = 0.0;
+        for (int j = 0; j < rows; j++)
+        {
+            sum += matr[j, i];
+        }
+        Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {sum / rows}");
+    }
+}
