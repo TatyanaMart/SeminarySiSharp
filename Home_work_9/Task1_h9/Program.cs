f6a77855db079e9e92e22384d4284bad07baac8c
Tatyana <sta-rzn62@yandex.ru>
2022-09-18 21:28:04 +0300

    Add Home_work_9

---
+++ Home_work_9/Task1_h9/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8"" */
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNumbers(m, n));
+
+string SumNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        return Convert.ToString(m);
+    }
+    else
+    {
+        if (m < n)
+        {
+            return m + " " + SumNumbers(m + 1, n);
+        }
+        else
+        {
+            return m + " " + SumNumbers(m - 1, n);
+        }
+    }
+}
---
+++ Home_work_9/Task2_h9/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна {(SumNumbers(m, n))}");
+
+int SumNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else
+    {
+        if (m < n)
+        {
+            return m + SumNumbers(m + 1, n);
+        }
+        else
+        {
+            return m + SumNumbers(m - 1, n);
+        }
+    }
+}
